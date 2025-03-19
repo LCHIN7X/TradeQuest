77b59d3b944be387b38188f4e9d0169c9825c0da
LCHIN7X <lawchinxuan@gmail.com>
2025-03-19 15:13:26 +0800

    3

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "TRADEQUEST"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
+
+    db.init_app()
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
+
