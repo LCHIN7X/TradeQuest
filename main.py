e1dcf2d58a290e660eb7901b3e3ea87b9dcb7750
LCHIN7X <lawchinxuan@gmail.com>
2025-03-19 15:11:06 +0800

    2

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from model import db
 
 DATABASE_NAME = 'database.db'
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "TRADEQUEST"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
