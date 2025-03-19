dddcb45f37822634e4c1c403e0e6ca2b3b9cc293
LCHIN7X <lawchinxuan@gmail.com>
2025-03-19 17:40:59 +0800

    10

--- main.py
+++ main.py
@@ -8,10 +8,22 @@ def create_app():
     app.config['SECRET_KEY'] = "TRADEQUEST"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
 
+    from user.models import User
+    from user.views import views
+    app.register_blueprint(views, url_prefix="/views")
+
     db.init_app()
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = "views.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader 
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
 
