20f582a412ef5d8ffcb6246cc4865ae514687c6b
LCHIN7X <lawchinxuan@gmail.com>
2025-03-20 22:49:16 +0800

    13

--- user/views.py
+++ user/views.py
@@ -78,7 +78,7 @@ def login():
                 login_user(user_in_db, remember=True)
 
                 
-                return render_template('home.html') 
+                return render_template('layout.html') #change after got home.html
             
             # if password is incorrect, flash error message
             else:
